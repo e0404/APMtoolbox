a6506e20c9de0a524372761fd8e85bf5e66fed70
Niklas Wahl <n.wahl@dkfz.de>
2018-05-23 17:26:30 +0200

    removed dependency on tensor toolbox from (co)variance gradient calculations

--- cProbOpt/apm_calcVarGrad.m
+++ cProbOpt/apm_calcVarGrad.m
@@ -1,5 +1,8 @@
 function varGrad = apm_calcVarGrad(Vijm,w)
-varGrad =  2*double(ttv(tensor(Vijm),w,3));
+%varGrad =  2*double(ttv(tensor(Vijm),w,3));
 %varGrad =  2*reshape(reshape(permute(Vijm,[1 3 2]),size(Vijm,1)*numel(w),numel(w)) * w,size(Vijm,1),numel(w));
+
+varGrad = 2*reshape(reshape(Vijm,size(Vijm,1)*numel(w),numel(w)) * w,size(Vijm,1),numel(w));
+
 end
 
