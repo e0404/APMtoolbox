af310e196510f6297a26de76ec31b9b6f8365cf6
fgesualdi <flavia.gesualdi@iteda.cnea.gov.ar>
2022-04-04 12:22:51 +0200

    Fix bug

--- cProbOpt/transforms/apm_transformMeanVarianceToBetaParameters.m
+++ cProbOpt/transforms/apm_transformMeanVarianceToBetaParameters.m
@@ -1,11 +1,8 @@
 function [alpha,beta] = apm_transformMeanVarianceToBetaParameters(mu,sigma)
-%This function gives alpha and beta parameters for a beta-distributed
-%variable, if their shape parameter is known.
+% This function gives alpha and beta parameters for a beta-distributed
+% variable, if their shape parameter is known.
 
-%alpha = (mu.^2 - mu.^3 - mu.*(sigma.^2))./ sigma.^2;
-%beta = (mu - 1) .* (mu.^2 - mu + sigma.^2) ./ sigma.^2;
-
-alpha = ((1 - mu.^2)./sigma.^2 - 1./mu) .* mu.^2;
+alpha = ((mu .* (1 - mu))./ (sigma.^2) - 1) .* mu;
 beta = alpha.*(1./mu - 1);
 
 if any(sigma.^2 > mu .* (1 - mu))
