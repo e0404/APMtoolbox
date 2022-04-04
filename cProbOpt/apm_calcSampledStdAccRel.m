5b4daea0e1e008361a106317b26ae118f5412a69
fgesualdi <flavia.gesualdi@iteda.cnea.gov.ar>
2022-04-04 17:05:16 +0200

    Modularize

---
+++ cProbOpt/apm_calcSampledStdAccRel.m
@@ -0,0 +1,8 @@
+function sampledStdAccRel = apm_calcSampledStdAccRel(n)
+
+Kn = @(n) sqrt((n-1)/2).*exp(gammaln((n-1)/2) - gammaln(n/2));
+Vn = @(n) 2*((n-1)/2 - exp(2*gammaln(n/2) - 2*gammaln((n-1)/2)));
+sampledStdAccRel = @(n) Kn(n).*sqrt(Vn(n))./sqrt(n-1);
+
+end
+
