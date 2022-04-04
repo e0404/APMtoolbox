a6f3696bf9679fa11f3a24cec9b40489aaaa8f48
fgesualdi <flavia.gesualdi@iteda.cnea.gov.ar>
2022-04-04 17:21:28 +0200

    Fix bug

--- cProbOpt/apm_calcSampledStdAccRel.m
+++ cProbOpt/apm_calcSampledStdAccRel.m
@@ -2,7 +2,7 @@ function sampledStdAccRel = apm_calcSampledStdAccRel(n)
 
 Kn = @(n) sqrt((n-1)/2).*exp(gammaln((n-1)/2) - gammaln(n/2));
 Vn = @(n) 2*((n-1)/2 - exp(2*gammaln(n/2) - 2*gammaln((n-1)/2)));
-sampledStdAccRel = @(n) Kn(n).*sqrt(Vn(n))./sqrt(n-1);
+sampledStdAccRel = Kn(n).*sqrt(Vn(n))./sqrt(n-1);
 
 end
 
